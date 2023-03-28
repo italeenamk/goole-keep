a32072a07571b47b1eb61708c4089d1c132f394e
italeenamk <italeenamk@gmail.com>
2023-03-28 22:34:32 +0500

    google-keep

---
+++ src/Home/Home.jsx
@@ -0,0 +1,163 @@
+import '../style/style.css';
+
+const Home = () => {
+  return(
+      <>
+          <section class="header-navbar-content">
+              <div class="container-fluid">
+                  <div class="row">
+                      <div class="col-md-2 col-sm-12">
+                          <div class="header-navbar-icon-list-content">
+                              <nav>
+                                  <ul>
+                                      <li><a href=""><i class="fa-solid fa-bars" title="Main Menu"></i></a></li>
+                                      <li><img src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png" alt="" title="Keep"/></li>
+                                      <li><a href="" title="Keep">Keep</a></li>
+                                  </ul>
+                              </nav>
+                          </div>
+                      </div>
+                      <div class="col-md-7 col-sm-12">
+                          <div class="header-navbar-input-content">
+                              <form>
+                                  <div class="input-group mb-3">
+                                      <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <a href=""><i class="fa-solid fa-magnifying-glass" title="Search"></i></a>
+                                    </span>
+                                      </div>
+                                      <input type="text" class="" placeholder="Search"/>
+                                      <div class="input-group-prepend input-remove-content">
+                                    <span class="input-group-text">
+                                        <a href=""><i class="fa-solid fa-x"></i></a>
+                                    </span>
+                                      </div>
+                                  </div>
+                              </form>
+                          </div>
+                      </div>
+                      <div class="col-md-3 col-sm-12 text-right">
+                          <div class="header-navbar-icon-list-content">
+                              <nav>
+                                  <ul class="justify-content-end">
+                                      <li><a href=""><i class="fa-solid fa-rotate-right"></i></a></li>
+                                      <li><a href=""><i class="fa-solid fa-bars-progress"></i></a></li>
+                                      <li><a href=""><i class="fa-solid fa-gear"></i></a></li>
+                                      <div class="nav-list">
+                                          <ul>
+                                              <li><a href=""><i class="fa-solid fa-arrow-up-right-dots"></i></a></li>
+                                              <li><a href=""><img class="navbar-user-img-content" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Eo_circle_deep-orange_white_letter-a.svg/640px-Eo_circle_deep-orange_white_letter-a.svg.png" alt=""/></a></li>
+                                          </ul>
+                                      </div>
+                                  </ul>
+                              </nav>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </section>
+          <section class="slider-content">
+              <div class="container-fluid">
+                  <div class="row">
+                      <div class="col-md-4 col-lg-3 col-sm-12">
+                          <div class="slide-bar-left-nav-content">
+                              <nav>
+                                  <ul>
+                                      <li><a href=""><i class="fa-regular fa-lightbulb"></i> Notes</a></li>
+                                      <li><a href=""><i class="fa-regular fa-bell"></i> Reminders</a></li>
+                                      <li><a href=""><i class="fa-solid fa-pen"></i> Edit labels</a></li>
+                                      <li><a href=""><i class="fa-solid fa-download"></i> Archive</a></li>
+                                      <li><a href=""><i class="fa-solid fa-trash-can"></i> Bin</a></li>
+                                  </ul>
+                              </nav>
+                          </div>
+                          <div class="slide-bar-left-bottom-link">
+                              <a href="">Open-source licences</a>
+                          </div>
+                      </div>
+                      <div class="col-md-8 col-lg-9 col-sm-12">
+                          <div class="left-side-colum-content">
+                              <div class="left-side-input-content">
+                                  <form>
+                                      <div class="input-group mb-3">
+                                          <input type="text" class="" placeholder="Take a note..."/>
+                                          <div class="input-group-prepend input-remove-content">
+                                    <span class="input-group-text">
+                                        <a href=""><i class="fa-solid fa-square-check"></i></a>
+                                    </span>
+                                              <span class="input-group-text">
+                                        <a href=""><i class="fa-solid fa-paintbrush"></i></a>
+                                    </span>
+                                              <span class="input-group-text">
+                                        <a href=""><i class="fa-regular fa-image"></i></a>
+                                    </span>
+                                          </div>
+                                      </div>
+                                  </form>
+                              </div>
+                          </div>
+                          <div class="left-side-colum-note-content">
+                              <div class="row">
+                                  <div class="col-md-3 mb-2 col-sm-12">
+                                      <div class="card-note">
+                                          <p>Hello</p>
+                                          <h3>You unlocked new Achievements with private contributions! Show them off by including private contributions in your Profile in</h3>
+                                      </div>
+                                  </div>
+                                  <div class="col-md-3 mb-2 col-sm-12">
+                                      <div class="card-note">
+                                          <p>Hello</p>
+                                          <h3>You unlocked new Achievements with private contributions! Show them off by including private contributions in your Profile in</h3>
+                                      </div>
+                                  </div>
+                                  <div class="col-md-3 mb-2 col-sm-12">
+                                      <div class="card-note">
+                                          <p>Hello</p>
+                                          <h3>You unlocked new Achievements with private contributions! Show them off by including private contributions in your Profile in</h3>
+                                      </div>
+                                  </div>
+                                  <div class="col-md-3 mb-2 col-sm-12">
+                                      <div class="card-note">
+                                          <p>Hello</p>
+                                          <h3>You unlocked new Achievements with private contributions! Show them off by including private contributions in your Profile in</h3>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="left-side-colum-note-content">
+                              <div class="row">
+                                  <div class="col-md-3 mb-2 col-sm-12">
+                                      <div class="card-note">
+                                          <p>Hello</p>
+                                          <h3>You unlocked new Achievements with private contributions! Show them off by including private contributions in your Profile in</h3>
+                                      </div>
+                                  </div>
+                                  <div class="col-md-3 mb-2 col-sm-12">
+                                      <div class="card-note">
+                                          <p>Hello</p>
+                                          <h3>You unlocked new Achievements with private contributions! Show them off by including private contributions in your Profile in</h3>
+                                      </div>
+                                  </div>
+                                  <div class="col-md-3 mb-2 col-sm-12">
+                                      <div class="card-note">
+                                          <p>Hello</p>
+                                          <h3>You unlocked new Achievements with private contributions! Show them off by including private contributions in your Profile in</h3>
+                                      </div>
+                                  </div>
+                                  <div class="col-md-3 mb-2 col-sm-12">
+                                      <div class="card-note">
+                                          <p>Hello</p>
+                                          <h3>You unlocked new Achievements with private contributions! Show them off by including private contributions in your Profile in</h3>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </section>
+      </>
+  )
+}
+
+export default Home;
